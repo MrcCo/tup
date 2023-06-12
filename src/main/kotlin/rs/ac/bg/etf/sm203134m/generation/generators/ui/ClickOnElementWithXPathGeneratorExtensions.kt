6eac3c70ab1d09e2befb6429a48928d4595b4d87
Marko Stefanovic <markostefanovic2801@gmail.com>
2023-06-12 20:11:36 +0200

    Extract g4 files to a separate repository

--- src/main/kotlin/rs/ac/bg/etf/sm203134m/generation/generators/ui/ClickOnElementWithXPathGeneratorExtensions.kt
+++ src/main/kotlin/rs/ac/bg/etf/sm203134m/generation/generators/ui/ClickOnElementWithXPathGeneratorExtensions.kt
@@ -2,7 +2,7 @@ package rs.ac.bg.etf.sm203134m.generation.generators.ui
 
 import rs.ac.bg.etf.sm203134m.antlr4.TupParser
 
-fun TupParser.ClickElementWithXPathContext.generateOnEntry(): String {
+fun TupParser.ClickOnElementWithXPathContext.generateOnEntry(): String {
 
     return "\t\tdriver.findElement(By.xpath(${this.STRING()})).click();\n"
 
